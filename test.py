e836fcc0377a442a3467d0253fb2d1adbaade7c8
byungs2 <byungs2@naver.com>
2020-10-22 17:24:32 +0900

    2020-10-22 backup

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-import numpy as np
-sizes = [2, 3, 1]
-print(sizes[1:])
-print(sizes[:-1])
-print(sizes[1:])
-print(zip(sizes[:-1],sizes[1:]))
-weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
-print(weights)
-biases = [np.random.randn(y, 1) for y in sizes[1:]]
-print(biases)
-print(biases[0].shape)
+x= 1
+y= 2
+print(int(x==y))
