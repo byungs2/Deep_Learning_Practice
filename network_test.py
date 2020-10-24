53396ba594ecb99a0193b7b0277408316be286ea
byungs2 <byungs2@naver.com>
2020-10-24 16:27:21 +0900

    2020-10-24 different data model of mnist problem fix

--- network_test.py
+++ network_test.py
@@ -1,15 +1,28 @@
 import network
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot= True)
+
+t = input_data.read_data_sets("MNIST_data/", one_hot= False)
+test_x = t.train.images[45000:]
+test_x_reshaped = [i.reshape(784,1) for i in test_x]
+test_y = t.train.labels[45000:]
 x = mnist.train.images
 y = mnist.train.labels
+
+t_y = [i.reshape(10,1) for i in y]
+t_x = [i.reshape(784,1) for i in x]
+
 training_data = []
 test_data = []
-for i, j in zip(x[:45000], y[:45000]) :
+
+for i, j in zip(t_x, t_y) :
     training_data.append((i,j))
 
-for i, j in zip(x[45000:], y[45000:]) :
+for i, j in zip(test_x_reshaped, test_y) :
     test_data.append((i, j))
 
-net = network.Network([784,30,10])
-net.SGD(training_data,30, 30, 0.001, test_data = test_data)
+net = network.Network([784,100,10])
+net.SGD(training_data,300, 30, 3.0, test_data = test_data)
+
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
-x= 1
-y= 2
-print(int(x==y))
+import network
+import numpy as np
+import pandas as pd
+
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot= True)
+
+df = pd.DataFrame(mnist)
+print(df.columns)
